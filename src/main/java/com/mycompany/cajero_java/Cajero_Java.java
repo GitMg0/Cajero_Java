771003199d0c5cf5b68b2452b3d90bff4f883aa5
GitMg0 <miguelgrajales9801@gmail.com>
2024-06-10 12:56:03 -0500

    Version 1.0

---
+++ src/main/java/com/mycompany/cajero_java/Cajero_Java.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Cajero_Java {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Hello World!");
+    }
+}
