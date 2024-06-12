bc3308c5c2a867a541fcc3fbe905f075de3afc64
GitMg0 <90349212+GitMg0@users.noreply.github.com>
2024-06-12 17:16:01 -0500

    Delete

--- src/main/java/com/mycompany/cajero_java/Cajero_Java.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Cajero_Java {
-
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Hello World!");
-    }
-}
